c16b248d74d75468c8fb4b07d7b49893c807b5df
Maximiliano Calero <93091450+podolsky14@users.noreply.github.com>
2022-02-12 20:41:34 -0300

    Update Index.js

--- src/components/Index.js
+++ src/components/Index.js
@@ -755,7 +755,7 @@ otable habilidad en el regate, gran juego aéreo a pesar de su corta estatura y 
 
         <tr  className="d-flex justify-content-center"><h5>JUGADOR POR GENERACIÓN</h5></tr>
         
- <div className="d-flex flex-column align-items-center py-5">       
+ <div className=" py-5">       
 .<th className="d-flex justify-content-center">1930-34</th>
 .<tr className="d-flex justify-content-center">GIUSEPPE MEAZZA (ITA)</tr>
 .<tr className="d-flex justify-content-center">MATTHIAS SINDELAR (AUS)</tr>
